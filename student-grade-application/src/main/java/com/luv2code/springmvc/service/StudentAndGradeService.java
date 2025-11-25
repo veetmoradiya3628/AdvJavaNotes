6c82fd9193708e3b362cf3a2efb94b2538d87499
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2025-11-25 22:33:01 +0530

    database integration testing added

---
+++ student-grade-application/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -0,0 +1,37 @@
+package com.luv2code.springmvc.service;
+
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class StudentAndGradeService {
+    @Autowired
+    private StudentDao studentDao;
+
+    public void createStudent(String firstName, String lastName, String emailAddress){
+        CollegeStudent student = new CollegeStudent(firstName, lastName, emailAddress);
+        student.setId(0);
+        studentDao.save(student);
+    }
+
+    public boolean checkIfStudentIsNull(int studentId) {
+        Optional<CollegeStudent> student = studentDao.findById(studentId);
+        return student.isPresent();
+    }
+
+    public void deleteStudent(int studentId) {
+        if (checkIfStudentIsNull(studentId)) {
+            studentDao.deleteById(studentId);
+        }
+    }
+
+    public Iterable<CollegeStudent> getGradeBook() {
+        return studentDao.findAll();
+    }
+}
---
+++ student-grade-application/src/main/resources/insertData.sql
@@ -0,0 +1,4 @@
+insert into student(id, firstname, lastname, email_address) values (11, 'Student', 'One', 'student.one@msi.com')
+insert into student(id, firstname, lastname, email_address) values (12, 'Student', 'Two', 'student.two@msi.com')
+insert into student(id, firstname, lastname, email_address) values (13, 'Student', 'Three', 'student.three@msi.com')
+insert into student(id, firstname, lastname, email_address) values (14, 'Student', 'Four', 'student.four@msi.com')
