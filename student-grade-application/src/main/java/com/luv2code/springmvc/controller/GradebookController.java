445a7d15a3d63600cb3ec67d9456c07e39f3f36e
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2025-11-29 00:29:01 +0530

    mvc controller testing added

--- student-grade-application/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ student-grade-application/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -1,6 +1,7 @@
 package com.luv2code.springmvc.controller;
 
 import com.luv2code.springmvc.models.*;
+import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +13,39 @@ public class GradebookController {
 	@Autowired
 	private Gradebook gradebook;
 
+    @Autowired
+    private StudentAndGradeService studentService;
+
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getStudents(Model m) {
+        Iterable<CollegeStudent> collegeStudents = studentService.getGradeBook();
+        m.addAttribute("students", collegeStudents);
 		return "index";
 	}
 
+    @PostMapping(value = "/")
+    public String createStudent(@ModelAttribute("student") CollegeStudent student, Model m) {
+        studentService.createStudent(student.getFirstname(), student.getLastname(), student.getEmailAddress());
+
+        Iterable<CollegeStudent> collegeStudents = studentService.getGradeBook();
+        m.addAttribute("students", collegeStudents);
+
+        return "index";
+    }
+
+    @GetMapping(value = "/delete/student/{id}")
+    public String deleteStudent(@PathVariable("id") int id, Model m){
+        if (!studentService.checkIfStudentIsNull(id)){
+            return "error";
+        }
+        studentService.deleteStudent(id);
+
+        Iterable<CollegeStudent> collegeStudents = studentService.getGradeBook();
+        m.addAttribute("students", collegeStudents);
+
+        return "index";
+    }
 
 	@GetMapping("/studentInformation/{id}")
 		public String studentInformation(@PathVariable int id, Model m) {
---
+++ student-grade-application/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -0,0 +1,139 @@
+package com.luv2code.springmvc;
+
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.models.GradebookCollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestPropertySource("/application.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+public class GradebookControllerTest {
+    private static MockHttpServletRequest request;
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private StudentDao studentDao;
+
+    @Mock
+    private StudentAndGradeService studentCreateServiceMock;
+
+    @BeforeAll
+    public static void setup() {
+        request = new MockHttpServletRequest();
+        request.setParameter("firstname", "Chad");
+        request.setParameter("lastname", "Darby");
+        request.setParameter("emailAddress", "chad.darby@luv2code_school.com");
+    }
+
+    @BeforeEach
+    public void beforeEach(){
+        jdbc.execute("insert into student(firstname, lastname, email_address) " +
+                "values('Eric', 'Roby', 'eric.roby@msi.com')");
+    }
+
+    @Test
+    public void getStudentHttpRequest() throws Exception {
+        CollegeStudent student1 = new GradebookCollegeStudent("Eric", "Roby", "eric_roby@luv2code_school.com");
+        CollegeStudent student2 = new GradebookCollegeStudent("Chad", "Darby", "chad_darby@luv2code_school.com");
+
+        List<CollegeStudent> collegeStudentList = new ArrayList<>(Arrays.asList(student1, student2));
+
+        when(studentCreateServiceMock.getGradeBook()).thenReturn(collegeStudentList);
+
+        assertIterableEquals(collegeStudentList, studentCreateServiceMock.getGradeBook());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        assertNotNull(mav);
+        ModelAndViewAssert.assertViewName(mav, "index");
+    }
+
+    @Test
+    public void createStudentHttpRequest() throws Exception {
+        CollegeStudent student1 = new CollegeStudent("Eric", "Roby", "eric_roby@luv2code_school.com");
+        List<CollegeStudent> collegeStudentList = new ArrayList<>(List.of(student1));
+        when(studentCreateServiceMock.getGradeBook()).thenReturn(collegeStudentList);
+        assertIterableEquals(collegeStudentList, studentCreateServiceMock.getGradeBook());
+
+        MvcResult mvcResult = this.mockMvc.perform(post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("firstname", Objects.requireNonNull(request.getParameterValues("firstname")))
+                        .param("lastname", Objects.requireNonNull(request.getParameterValues("lastname")))
+                        .param("emailAddress", Objects.requireNonNull(request.getParameterValues("emailAddress"))))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+        assertNotNull(mav);
+        ModelAndViewAssert.assertViewName(mav, "index");
+
+        CollegeStudent verifyStudent = studentDao.findByEmailAddress("chad.darby@luv2code_school.com");
+        assertNotNull(verifyStudent, "student should be found!");
+    }
+
+    @Test
+    public void deleteStudentHttpRequest() throws Exception {
+        assertTrue(studentDao.findById(1).isPresent());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .get("/delete/student/{id}", 1))
+                        .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mvc = mvcResult.getModelAndView();
+
+        assertNotNull(mvc);
+        ModelAndViewAssert.assertViewName(mvc, "index");
+
+        assertFalse(studentDao.findById(1).isPresent());
+    }
+
+    @Test
+    public void deleteStudentHttpRequestErrorPage() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .get("/delete/student/{id}", -1))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        assertNotNull(mav);
+        ModelAndViewAssert.assertViewName(mav, "error");
+    }
+
+    @AfterEach
+    public void setupAfterTransaction() {
+        jdbc.execute("DELETE FROM student");
+        jdbc.execute("ALTER TABLE student ALTER COLUMN ID RESTART WITH 1");
+    }
+}
